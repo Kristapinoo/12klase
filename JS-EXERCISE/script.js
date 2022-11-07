8e03be27c56c4abad4a3945c81193e3b7b279ae4
unknown <kristapskostukevics@gmail.com>
2022-11-07 15:29:01 +0200

    07_11
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ JS-EXERCISE/script.js
@@ -0,0 +1,10 @@
+const firstName = "Jānis";
+
+// tajā brīdī, kad dators redz const vai, tas saprot, ka tlt tiks definēts mainīgais
+// pēc const nāk mainīgā nosaukums
+// tas piešķir vērtību
+
+
+const firstNameRepeared = firstName.repeat(5)
+
+console.log(firstName.repeat(5))
