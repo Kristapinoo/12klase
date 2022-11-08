3ed5b68733eb2c60dc935480e984d42c49f933c7
unknown <kristapskostukevics@gmail.com>
2022-11-08 14:51:00 +0200

    08_11
    Vēl paspēlējāmies ar Math funckijām

---
+++ JS-EXERCISE/07_11/script.js
@@ -0,0 +1,50 @@
+// const firstName = "Jānis";
+
+// // tajā brīdī, kad dators redz const vai let, tas saprot, ka tlt tiks definēts mainīgais
+// // pēc const nāk mainīgā nosaukums
+// // tas piešķiram vērtību
+
+// let repeatingCount = 5
+
+// const firstNameRepeared = firstName.repeat(repeatingCount)
+
+// console.log(firstNameRepeared)
+
+// // mainīt šādi var tikai let, constus nevar
+// repeatingCount = 10
+
+// const firstNameRepearedMore = firstName.repeat(repeatingCount)
+
+// console.log(firstNameRepearedMore)
+//const result = num1++ ir tas pats kas x1 = x1+1
+
+// const num1 = 78.633056
+
+// const num2 = 6
+
+// const result =  Math.PI
+
+
+
+// console.log(Math.round(num1))
+
+// Objeks ir datu tips, kurā var būt daudz citu vērtību un dažādu datu tipu
+// Violētais ir astlēga?
+
+const ourFirstDatoruimObject = {
+    classNo: 12,
+    numberOfStudents: 150,
+    skullEmoji:"💀",
+    sunEmoji:"🌞",
+    level: 'Advanced',
+    mood: 'good',
+    money: null
+}
+
+ourFirstDatoruimObject.skullEmoji
+
+const allDatoriumObjects = {
+    firstProject :ourFirstDatoruimObject
+}
+
+console.log(allDatoriumObjects.firstProject.sunEmoji)
