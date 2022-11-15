38d1187f01b5219e1fdd93eefeee3f390fb32cd1
unknown <kristapskostukevics@gmail.com>
2022-11-15 15:10:14 +0200

    15_11
    Tas ir arī viss

---
+++ JS-EXERCISE/15_11/script.js
@@ -0,0 +1,84 @@
+// const country = 'USA'
+// let currency = 'USD'
+
+// if(country!=='USA') {
+//     currency = 'EUR'
+// }
+
+// console.log((currecy));
+
+//Global scope - window object (var)
+// var firstName = 'Kristaps'
+
+//Funciton scope - (let const var)
+// function sum() {
+//     const num = 5
+
+//     console.log(num); //5
+
+// }
+// sum()
+// console.log(num) // undefined
+
+//Block scope - {} (const un let)
+    // console.log()
+
+    // if(true) {
+    //     console.log()
+    // }
+
+
+// const allVariable = {
+
+//     nickname = 'Kristapinoo'
+//     game = 'Valorant'
+//     country = 'Latvia'
+
+// }
+
+// let text =`My nickname is ${nickname} and first game I played was ${game}`
+// console.log(text)
+
+// console.log(allVariable)
+
+// let number = 1
+// console.log(number)
+
+// number+=10
+// console.log(number)
+
+// number-=1
+// console.log(number)
+
+// const c= 'Mans'
+// let a = 'kakis'
+// var w = 'CJ'
+
+// console.log(c,a,w)
+
+// if(true) {
+//     // const condition = 'Jauns'
+//     // let animal = 'suns'
+//     var age = 3
+// }
+
+// console.log(age)
+
+// function scopeFunction()
+// {
+//     const a = 0;
+//     let b = 1;
+//     var c = 2;
+    
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// console.log(scopeFunction)
+
+function sayHello () {
+    console.log('Hello there, Jāni')
+
+}
+console.log(sayHello())
