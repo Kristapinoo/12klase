54b322ca9ea30153a487685ebb27eb58a0ec1a93
unknown <kristapskostukevics@gmail.com>
2022-11-22 15:13:55 +0200

    22_11
    Agha jaunas lietas

--- JS-EXERCISE/21__11/script.js
+++ JS-EXERCISE/21__11/script.js
@@ -24,17 +24,3 @@
 //     console.log(`I am in the loop with index: ${i}`);
 // }
 
-for (let i = 1; i <=100; i = i + 1) {
-    if (i % 3 == 0) {
-        console.log(`${i}. Fizz`);
-    }
-
-    if (i % 5 == 0) {
-        console.log(`${i}. Buzz`);
-    }
-
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log('${i}. FizzBuzz');
-    }
-
-}
---
+++ JS-EXERCISE/22_11/script.js
@@ -0,0 +1,84 @@
+// for (let i = 1; i <=100; i += 1) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz')
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz')
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+const ourArray = [
+    'orange', // index 0
+    'apple', // index 1
+    'peach', // index 2
+    'melon', // index 3
+    'kiwi', // index 4
+    'pear', // index 5
+    'banana', // index 6
+    'pineapple', // index 7
+    'watermelon', // index 8
+    'dragonfruit', // index 9
+    'clementine', // index 10
+    'fig', // index 11
+    'strawerry', // index 12
+    'cherry', // index 13
+    'mandarin', // index 14
+    'pomegranate', // index 15
+    'blackberry', // index 16
+    'lychee', // index 17
+    'pomelo', // index 18
+    'mango', // index 19
+    'eggplant', // index 20
+    'lemon', // index 21
+    'passionfruit', // index 22
+    'hurma', // index 23
+    'grapes', // index 24
+    'lime', // index 25
+    'grapefruit', // index 26
+]
+
+//     const sortedArray = ourArray.sort()
+//     // console.log(sortedArray)
+
+
+// // console.log(sortedArray.includes('Lime'))
+
+// function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// }
+  
+// for (let i = 0; i < sortedArray.length; i += 1) {
+//     const currentFruit = sortedArray[i]
+//     const capitalFruit = capitalizeFirstLetter(currentFruit)
+//     const fruitLen = capitalFruit.length
+//     // console.log(`Name of fruit ${capitalFruit} and the word contains ${fruitLen}`)
+//     if (fruitLen >= 5) {
+//         console.log(capitalFruit)
+//     }
+// }
+
+// const itemsLessThanFiveLetters = ourArray.filter((fruit, index) => {
+//     return (fruit.length < 5)
+// })
+
+// ourArray.forEach((fruit) => {
+//     console.log(fruit)
+// })
+
+// const modifiedFruits = ourArray.map((Fruit) => {
+//     return {
+//         name: Fruit,
+//         letters: Fruit.length,
+//     }
+// })
+
+// console.log(modifiedFruits);
+// .forEach() // Iet cauri katra masīva itemam
+// .map() // Modificē katra masīva itemu un atgriež masīvu
+// .filter() // Samazina masīvam izmēru un atgriež jaunu masīvu
+
+
