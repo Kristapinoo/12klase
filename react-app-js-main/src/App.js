bf3eea35d16af642c21784ebe1099ae720d72abd
unknown <kristapskostukevics@gmail.com>
2023-01-10 15:24:02 +0200

    10.01
    AAAAAAAAAA

--- react-app-js-main/src/App.js
+++ react-app-js-main/src/App.js
@@ -1,25 +1,84 @@
+import ColorBox from './components/colorBox/colorBox.js'
+
 import './App.css';
-import AudiR8 from './images/audi_r8.jpg'
 
 const carObject = {
   name: "Audi R8",
-  imageURL: "http://localhost:3000/static/media/audi_r8.3603ca61acb90ab8b0ca.jpg",
-  description:"jk;sdFBjkdshfuiserafsldfijughdfisugoaisdufgiuasdhdsfgdsigfiusdfgiudsgfiusdfygiysd",
-  inStock: true
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/R8_Coupe_V10_performance-1.jpg/2560px-R8_Coupe_V10_performance-1.jpg",
+  description: "The Audi R8 is a mid-engine, 2-seater sports car, which uses Audi's trademark quattro permanent all-wheel drive system. It was introduced by the German car manufacturer Audi AG in 2006.",
+  inStock: false,
+  isLuxus: false,
+  color: [
+    {
+      name: 'Tomato Red',
+      colorCode: '#C34A2C'
+    },
+    {
+      name: 'Panther Black',
+      colorCode: '#0f0d0d'
+    },
+    {
+      name: 'Nardo Gray',
+      colorCode: '#686A6C'
+    },
+    {
+      name: 'Phthalo Green',
+      colorCode: '#0E2F19'
+    },
+
+  ]
 }
+
 function App() {
+
+
   return (
     <div className="car">
-      <h1 className='car__title'>{carObject.name}</h1>
-      <img src={carObject.imageURL} alt="Audi r8" className='car__image' />
-      <p className='car__info'>{carObject.description}
+      <h1 className='car__title'>
+        {carObject.name}
+        {carObject.isLuxus && (
+          <span className='car__star'>
+            ⭐️
+          </span>
+        )}
+      </h1>
+      <img 
+        src={carObject.imageUrl}
+        alt="Audi r8" 
+        className='car__image'
+       />
+      <p className='car__info'>
+        {carObject.description}
       </p>
-      
-      <span class='car__stock'>
-      {carObject.inStock ? "In Stock" : "Not in stock"}
+      <span className={carObject.inStock ? "car__stock" : "car__not-in-stock"}>
+        {carObject.inStock ? "In stock" : "Not in stock"}
       </span>
+
+      <h4 className="car__title2">
+        Available colors
+      </h4>
+      <div className="car__colors">
+        <ColorBox
+          colorName={carObject.color[0].name}
+          colorCode={carObject.color[0].colorCode}
+        />
+        <ColorBox
+          colorName={carObject.color[1].name}
+          colorCode={carObject.color[1].colorCode}
+        />
+        <ColorBox
+          colorName={carObject.color[2].name}
+          colorCode={carObject.color[2].colorCode}
+        />
+        <ColorBox
+          colorName={carObject.color[3].name}
+          colorCode={carObject.color[3].colorCode}
+        />
+
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ react-app-js-main/src/components/colorBox/colorBox.js
@@ -0,0 +1,21 @@
+import './colorBox.css'
+
+
+function ColorBox({colorName, colorCode}) {
+
+    return (
+        <div className="color-box">
+            <span className="color-box__color-name">
+                {colorName}
+            </span>
+            <span 
+            className="color-box__color-name-box" 
+            style={{
+                backgroundColor: colorCode
+            }}
+            ></span>
+        </div>
+    )
+}
+
+export default ColorBox
