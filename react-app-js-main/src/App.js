5a1af581302849f859e822aed906c45a86e90591
unknown <kristapskostukevics@gmail.com>
2023-01-17 15:14:49 +0200

    17_01
    sdfghjk

--- react-app-js-main/src/App.js
+++ react-app-js-main/src/App.js
@@ -1,14 +1,13 @@
 import ColorBox from './components/colorBox/colorBox.js'
-import Counter from './components/counter/counter.js'
 
 import './App.css';
+import { useState } from 'react';
 
-
-const carObject = {
+const defaulCarObject = {
   name: "Audi R8",
   imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/R8_Coupe_V10_performance-1.jpg/2560px-R8_Coupe_V10_performance-1.jpg",
   description: "The Audi R8 is a mid-engine, 2-seater sports car, which uses Audi's trademark quattro permanent all-wheel drive system. It was introduced by the German car manufacturer Audi AG in 2006.",
-  inStock: true,
+  inStock: false,
   isLuxus: true,
   color: [
     {
@@ -30,15 +29,24 @@ const carObject = {
   ]
 }
 
-function App() {
+// Mēs gribam lai uzspiežot uz car__title, tiek mainīts mašīnas objekta staitā 
+// isLuxus uz pretējo, attiecīgi lai majaslapā parādās vai paslēpjās zvaigznīte
 
+function App() {
+  const [carObject, setCarObject] = useState(defaulCarObject)
 
   return (
     <div>
-      <Counter />
-
       <div className="car">
-        <h1 className='car__title'>
+        <h1 
+          className='car__title'
+          onClick={() => {
+            setCarObject({
+             ...carObject,
+             isLuxus: !carObject.isLuxus
+            })
+         }}
+        >
           {carObject.name}
           {carObject.isLuxus && (
             <span className='car__star'>
@@ -54,16 +62,25 @@ function App() {
         <p className='car__info'>
           {carObject.description}
         </p>
-        <span className={carObject.inStock ? "car__stock" : "car__not-in-stock"}>
-          {carObject.inStock ? "In stock" : "Not in stock"}
-        </span>
+        <div className='car__stock-wrapper'>
+          <button onClick={() => {
+             setCarObject({
+              ...carObject,
+              inStock: !carObject.inStock
+             })
+          }}>
+            Change
+          </button>
+          <span className={carObject.inStock ? "car__stock" : "car__not-in-stock"}>
+            {carObject.inStock ? "In stock" : "Not in stock"}
+          </span>
+        </div>
 
         <h4 className="car__title2">
           Available colors
         </h4>
         <div className="car__colors">
           {carObject.color.map((color) => {
-
               return (
                 <ColorBox
                   colorName={color.name}
@@ -72,6 +89,21 @@ function App() {
               )
             })}
         </div>
+        <from className="car__form">
+          <label className='car__form-label'>
+            Color name
+            <input type="text" placeholder='Tomato red...'
+            />
+            
+          </label>
+          <label className='car__form-label'>
+            Color
+            <input type="color" />
+          </label>
+          <button>
+            Add color
+          </button>
+        </from>
       </div>
     </div>
   );
--- react-app-js-main/src/components/counter/counter.js
+++ react-app-js-main/src/components/counter/counter.js
@@ -3,6 +3,12 @@ import './counter.css'
 
 
 const Counter = () => {
+    // izveidojam state, kurā varēsim veidot dažādus stāvokļus mūsu komonentam
+    // name - state nosaukums
+    // setName - funkcija, kas ļauj mainīt state
+    // ('') - iekavās, sākuma vērtība
+    // IK REIZI KAD STATE TIEK MAINĪTS KOMPONENTS TIEK PĀRZĪMĒTS
+    const [name, setName] = useState("")
     const [count, setCount] = useState(0)
     const [person, setPerson] = useState({
         name: 'Jānis',
@@ -12,9 +18,23 @@ const Counter = () => {
 
     return (
         <div className='counter'>
+            <h2>My name is: {name}</h2>
+            <input 
+                type="text" 
+                placeholder='Type your name'
+                value={name}
+                // kad nomaina inputu, mums nāk līdz eventa objekts, 
+                // kurā ir informācija par eventu
+                onChange={(eventObject) => {
+                    // eventa objektam var tikt klāt ievades vērtībai vaur target.value keyiem
+                    setName(eventObject.target.value)
+                }}
+            />
+            <br /><br /><br />
             <h1>{count}</h1>
             <button 
                 onClick={() => {
+                    // mainam steitu, kur jaunais satate būs vecais + 1
                     setCount(count + 1)
                 }}
             >
@@ -36,31 +56,29 @@ const Counter = () => {
             <h1>Person:</h1>
             <h2>Person name is: {person.name}</h2>
             <h2>Person age is: {person.age}</h2>
+            <h2>Person likes cats: {person.likeCats ? 'yes' : 'no'}</h2>
             <button 
-            onClick={() => {
-                setPerson({
-                    name: 'John',
-                    age: person.age,
-                    likeCats: person.likeCats,
-                })
-            }}
+                onClick={() => {
+                    setPerson({
+                    // ... saucās spread operators, kurš paņem person objekta vērtības un "ielej" jaunajā objektā
+                        ...person,
+                        name: 'John',
+                    })
+                }}
             >
                 Change name to John
             </button>
             <button onClick={() => {
                 setPerson({
-                    likeCats: person.likeCats,
-                    name: person.name,
+                    ...person,
                     age: person.age + 1
                 })
             }}>
                 Add +1 to age
             </button>
+            <br /><br /><br />
         </div>
     )
 }
 
 export default Counter
-
-// events - click, mouseover, sumbit
-// state - 
