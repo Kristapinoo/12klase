749a39b713670be89dac5ae55662dbcbf377ac06
unknown <kristapskostukevics@gmail.com>
2023-02-07 15:15:12 +0200

    07.02.
    iofjsoifjkhbsdrfagkbjhlsdfg

--- kaut-kadi-boiler-main/react-app-js-main/src/App.js
+++ kaut-kadi-boiler-main/react-app-js-main/src/App.js
@@ -1,33 +1,57 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
+  const [autors, setAutors] = useState([])
+  const [newAuthor, setNewAuthor] = useState("")
 
-  useEffect(() => {
-    axios.get('http://localhost:3004/')
-    .then((data) => {
-      console.log(data)
+  const fetchAllAutors = () => {
+    axios.get('http://localhost:3004/autors').then((response) => {
+
+      setAutors(response.data)
     });
+  }
+
+  // izskauksies vienu reizi uz komponenta ielādi
+  useEffect(() => {
+    fetchAllAutors()
   }, [])
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+
+          axios.post('http://localhost:3004/autors', {name: newAuthor}).then(() => {
+            fetchAllAutors()
+
+            setNewAuthor('')
+          });
+
+        }}>
+          <input 
+            required
+            placeholder='Author name...'
+            type="text"
+            value={newAuthor}
+            onChange={(e) => {
+              setNewAuthor(e.target.value)
+            }}
+          /> 
+          <br />
+          <button>
+            Add new author
+          </button>
+        </form>
+
+        <div>
+          {autors.map((autors) => (
+            <h2 key={autors.id}>{autors.name}</h2>
+          ))}
+        </div>
       </header>
     </div>
   );
--- kaut-kadi-boiler-main/sqllite_js_boilerplate-main/src/server.js
+++ kaut-kadi-boiler-main/sqllite_js_boilerplate-main/src/server.js
@@ -6,57 +6,112 @@ const sqlite3 = require('sqlite3').verbose();
 // pieslēdzamies mūsu DB
 const database = new sqlite3.Database("./src/db/database.db");
 
+// inicializējam express appu
 const app = express()
-const port = 3004
 
 // ļaujam piekļūt serverim no citiem domēniem
 app.use(cors({
   origin: '*'
 }))
 
+// ļaujam no FE sūtīt jsonu
 app.use(bodyParser.json());
 
+// uz servera palaišanu
 database.serialize(() => {
+  // izveidojam autors tabulu ja tāda neeksistē
   database.run(`
     CREATE TABLE IF NOT EXISTS autors (
-      id int,
-      name VARCHAR(255) NOT NULL,
-      PRIMARY KEY (id)
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
     );
-  
+  `);
+
+  // izveidojam gleznas tabulu ja tāda neeksistē
+  database.run(`
     CREATE TABLE IF NOT EXISTS gleznas (
-      id int NOT NULL,
+      id INTEGER PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       imgSrc char NOT NULL,
       author_id int,
-      PRIMARY KEY (id),
       FOREIGN KEY (author_id) REFERENCES autors(id)
     ); 
   `);
 
-  database.run(`
-    INSERT INTO autors (name)
-    VALUES('Bud Powell');
-  `);
 
-  database.run('SELECT * from autors', (result) => {
-    console.log('result', result);
-})
+  // gribam jau pašā sākumā kad izveidojam datubāzes, iesetot pirmos datus, 
+  // tāpēc paprasa datus no autors tabulas lai pārliecinātos vai tabula ir tukša vai nav
+  // dati būs pieejami funkcijas argumentā autors
+  database.get('SELECT * from autors', (err, autors) => {
+    // ja autoru nav, tad zinām ka datu vēl datubāzē nav vispār
+    if (!autors) {
+      // ieliekam iekšā pirmo autoru
+      database.run(`
+        INSERT INTO autors (name)
+        VALUES('Leonardo da Vinci');
+    `, () => {
+      // kad autors ir ielikt, ieliekam iekšā pirmo gleznu
+      database.run(`
+        INSERT INTO gleznas (name, imgSrc, author_id)
+        VALUES(
+          'Mona Lisa', 
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg',
+          1
+        );
+      `)
 
+      });
+    }
+  })
 });
+
 
 // controlieris kurš atbild par to, kad tiks prasīts GET piepeasījums uz root, 
 // jeb šajā gadījumā http://localhost:3004/
 app.get('/', (req, res) => {
 
-  res.json({name: 'John'})
+  // izvēlamies visuu datus no autors tabulas
+  database.get(`SELECT * FROM autors`, (err, movies) => {
+    // kad dati ir atnākuši no autors tabulas tad prasam datus no gleznas tabulas
+    database.get(`SELECT * FROM gleznas`,(err, gleznas) => {
+      // sūtam prom datus uz frontendu
+      res.json({
+        movies: movies, 
+        gleznas: gleznas
+      })
+    })
 
-  // database.get(`SELECT * FROM movies`,(err, row) => {
-  //   res.json(row);
-  // })
+  })
 })
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/autors', (req, res) => {
+    // database.get atgriež tikai vienu pirmo atrasto rezutlātu
+      // database.all atgriež visus atrastos rezultātus
+  database.all('SELECT * FROM autors', (error, autori) => {
+    res.json(autori)
+  })
+})
+
+app.post('/autors', (req, res) => {
+  database.run(`
+    INSERT INTO autors (name)
+    VALUES("${req.body.name}");
+  `, () => {
+    res.json('Jauns autors pievienots veiksmīgi')
+  })
+
+})
+
+
+// GET http://localhost:3004/autors
+// Atgriež visus autorus no DB
+
+// POST http://localhost:3004/autors
+// pievieno jaunu autoru
+
+
+// palaižam serveri ar 3004 portu
+app.listen(3004, () => {
+  console.log(`Example app listening on port 3004`)
 })
